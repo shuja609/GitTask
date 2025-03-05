7895d1e126747996fe411cf83d9f8eafa9313187
Amna Hassan <shujaqurashi2172@gmail.com>
2025-03-05 20:56:47 +0500

    Initial commit: Project bootstrap

---
+++ script.js
@@ -0,0 +1,7 @@
+function showMessage() {
+    alert("Button clicked! This is a message from the JavaScript function.");
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Document is fully loaded and parsed.");
+});
